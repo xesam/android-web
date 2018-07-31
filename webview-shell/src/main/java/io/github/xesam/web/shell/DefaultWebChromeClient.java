76b9f562715fac31f956ea154ae96b63381222b3
xesam <xesamguo@gmail.com>
2018-07-31 16:23:13 +0800

    DefaultWebChromeClient

--- webview-shell/src/main/java/io/github/xesam/web/shell/DefaultWebChromeClient.java
+++ webview-shell/src/main/java/io/github/xesam/web/shell/DefaultWebChromeClient.java
@@ -1,5 +1,7 @@
 package io.github.xesam.web.shell;
 
+import android.util.Log;
+import android.webkit.ConsoleMessage;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
 
@@ -8,5 +10,38 @@ public class DefaultWebChromeClient extends WebChromeClient {
     public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
         super.onGeolocationPermissionsShowPrompt(origin, callback);
         callback.invoke(origin, true, false);
+    }
+
+    @Override
+    public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+        if (consoleMessage != null) {
+            Log.d("DefaultWebChromeClient", "");
+            ConsoleMessage.MessageLevel messageLevel = consoleMessage.messageLevel();
+            String tag = "DefaultWebChromeClient";
+            String msg = buildConsoleMessage(consoleMessage);
+            switch (messageLevel) {
+                case TIP:
+                    Log.v(tag, msg);
+                    break;
+                case LOG:
+                    Log.i(tag, msg);
+                    break;
+                case DEBUG:
+                    Log.d(tag, msg);
+                    break;
+                case WARNING:
+                    Log.w(tag, msg);
+                    break;
+                case ERROR:
+                    Log.e(tag, msg);
+                    break;
+            }
+
+        }
+        return super.onConsoleMessage(consoleMessage);
+    }
+
+    private String buildConsoleMessage(ConsoleMessage message) {
+        return message.lineNumber() + ";" + message.sourceId() + ";" + message.message();
     }
 }
